10a6d8ec74ac871162b8490faa767a24bc2d8273
Ahmedbo9 <ahmed.benothman@live.fr>
2023-12-29 11:04:27 +0100

    detecting both hands landmarks

---
+++ main.py
@@ -0,0 +1,33 @@
+import time
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        continue
+
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+
+    # Check if any hands are detected and draw landmarks
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    # Display the frame
+    cv2.imshow('frame', frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release resources
+cap.release()
+cv2.destroyAllWindows()
+
