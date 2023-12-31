26b298f727ef38dfc8ca4f1bd4000da9ed6e7543
Ahmedbo9 <ahmed.benothman@live.fr>
2023-12-31 09:42:50 +0100

    adding find coordinate function

--- hand_tracking_module.py
+++ hand_tracking_module.py
@@ -1,8 +1,10 @@
 import cv2
 import mediapipe as mp
 
+
 class HandTracker:
     def __init__(self):
+        self.results = None
         self.cap = cv2.VideoCapture(0)
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands()
@@ -14,10 +16,10 @@ class HandTracker:
             return None
 
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(image)
+        self.results = self.hands.process(image)
 
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for hand_landmarks in self.results.multi_hand_landmarks:
                 # Draw all landmarks
                 self.mp_draw.draw_landmarks(frame, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
 
@@ -25,12 +27,22 @@ class HandTracker:
                     for id_, lm in enumerate(hand_landmarks.landmark):
                         h, w, c = frame.shape
                         cx, cy = int(lm.x * w), int(lm.y * h)
-
                         if id_ in landmark_ids:
-                            cv2.circle(frame, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+                            cv2.circle(frame, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
 
         return frame
 
+    def find_coordinate(self, frame, handNo=0, draw=True, landmark_id=None):
+        landmarks_list = []
+        if self.results.multi_hand_landmarks:
+            hand = self.results.multi_hand_landmarks[handNo]
+            for id_, lm in enumerate(hand.landmark):
+                h, w, c = frame.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                landmarks_list.append((cx, cy))
+                if draw:
+                    cv2.circle(frame, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+        return landmark_id, landmarks_list[landmark_id] if len(landmarks_list) > 0 else []
+
     def release(self):
         self.cap.release()
-
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ hand_tracker = HandTracker()
 
 while True:
     frame = hand_tracker.process_frame()
+    coords = hand_tracker.find_coordinate(frame, landmark_id=4)
+    print(coords)
     if frame is not None:
         cv2.imshow('frame', frame)
 
