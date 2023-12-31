5297416986e9159cf1d23c367124f0dbacbb2c96
Ahmedbo9 <ahmed.benothman@live.fr>
2023-12-31 10:22:43 +0100

    minor refactor

--- hand_tracking_module.py
+++ hand_tracking_module.py
@@ -5,12 +5,12 @@ import mediapipe as mp
 class HandTracker:
     def __init__(self):
         self.results = None
-        self.cap = cv2.VideoCapture(0)
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands()
         self.mp_draw = mp.solutions.drawing_utils
+        self.cap = None
 
-    def process_frame(self, landmark_ids=None):
+    def process_frame(self, landmark_ids=None ):
         ret, frame = self.cap.read()
         if not ret:
             return None
@@ -41,7 +41,7 @@ class HandTracker:
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 landmarks_list.append((cx, cy))
                 if draw:
-                    cv2.circle(frame, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+                    cv2.circle(frame, (cx, cy), 12, (255, 0, 255), cv2.FILLED)
         return landmark_id, landmarks_list[landmark_id] if len(landmarks_list) > 0 else []
 
     def release(self):
--- main.py
+++ main.py
@@ -4,15 +4,22 @@ from hand_tracking_module import HandTracker
 
 hand_tracker = HandTracker()
 
+WCam, HCam = 640, 480
+cap = cv2.VideoCapture(0)
+cap.set(3, WCam)
+cap.set(4, HCam)
+hand_tracker.cap = cap
+
+
 while True:
     frame = hand_tracker.process_frame()
     coords = hand_tracker.find_coordinate(frame, landmark_id=4)
     print(coords)
     if frame is not None:
         cv2.imshow('frame', frame)
+        cv2.waitKey(1)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-hand_tracker.release()
-cv2.destroyAllWindows()
+
